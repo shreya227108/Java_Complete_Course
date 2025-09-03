e49399579c955fefc2d4430d1b9f9e587fa3941b
Shreya Patil <144721490+shreya227108@users.noreply.github.com>
2025-09-03 23:56:47 +0530

    Add files via upload

---
+++ Practice_Set_04/Palindrome.java
@@ -0,0 +1,23 @@
+package Day_05_Patterns.Practice_Set_04;
+
+public class Palindrome {
+    public static void main(String[] args) {       int n = 5;
+        for(int i=1; i<=n; i++) {
+            //spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            //first part
+            for(int j=i; j>=1; j--) {
+                System.out.print(j);
+            }
+
+            //second part
+            for(int j=2; j<=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Practice_Set_04/rhombus.java
@@ -0,0 +1,18 @@
+package Day_05_Patterns.Practice_Set_04;
+
+public class rhombus {
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 1; j <= n-i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
